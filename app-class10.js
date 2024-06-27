fd7ec36d39864b0cdcdd9c0ed7888e91eb23726f
Davi Bernardo <davi.bernardo@sbahq.org>
2024-06-27 18:19:08 -0300

    VueJs - v for

---
+++ app-class10.js
@@ -0,0 +1,22 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        names: ['Davi', 'Nathan', 'Silvia', 'Carlos', 'Sandra', 'Elias'],
+        fruits: [
+            {
+                name:'Laranja', color:'Laranja', weight:10,
+            },
+            {
+                name:'Abacate', color:'Verde', weight:20,
+            },
+            {
+                name:'Manga', color:'Amarela', weight:25,
+            },
+            {
+                name:'Uva', color:'Roxo', weight:5
+            }
+        ]
+    },
+    methods: {
+    },
+})
