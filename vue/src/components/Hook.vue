ce6e7040ff9206320203ac3dd8f6d5a57a71de49
Davi Bernardo <davi.bernardo@sbahq.org>
2024-07-01 17:01:55 -0300

    VueJs Axios Request

--- vue/src/components/Hook.vue
+++ vue/src/components/Hook.vue
@@ -1,41 +1,41 @@
 <template>
     <div class="hook">
-        <h1>{{ title }}</h1>
-        <button @click="changeTitle">Clique aqui</button>
+        <h1>Blog</h1>
+        <hr>        
+        <div v-for="post in posts" :key="post.id">
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.body }}</p>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'Hook',
     data(){
         return {
-            title: 'Dados principais Hook'
+            posts:[]
         }
     },
     methods:{
-        changeTitle(){
-            this.title = 'Projeto VueJs'
-        }
+       
     },
-    beforeCreate(){
-        alert('Before Create Hook')
-    },
-    created() {
-        alert('Created Hook')
-    },
-    beforeMount(){
-        alert('Before Mount Hook')
-    },
-    mounted() {
-        alert('Mounted Hook')
-    },
-    beforeUpdate(){
-        alert('Before update message')
+    created(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            this.posts = response.data
+        })
+        .catch(error => {
+            console.log(error)
+        })
     }
 }
 </script>
 
 <style>
- 
+    h1{
+        color: red;
+        text-align: center;
+    }
 </style>
