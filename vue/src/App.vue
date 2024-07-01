03fa041e98fd2340e3c956b202fab6c34dd0cdb1
Davi Bernardo <davi.bernardo@sbahq.org>
2024-07-01 10:59:27 -0300

    VueJs Props

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,29 +2,34 @@
   <div id="app">
     <h2>{{ title }}</h2>
     <Navbar></Navbar>
-    <nav>
-      <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">Post</a></li>
-          <li><a href="">Sobre</a></li>
-          <li><a href="">Contato</a></li>
-      </ul>
-    </nav>
+    <AllFriends :friends="friends"></AllFriends>
+    <OnlineFriends :friends="friends"></OnlineFriends>
   </div>  
 </template>
 
 <script>
 import Navbar from './components/Navbar';
+import AllFriends from './components/AllFriends';
+import OnlineFriends from './components/OnlineFriends';
 
 export default {
   name: 'App',
   data(){
     return {
-      title: 'Uau, nosso site vue está funcionando'
+      title: 'Uau, nosso site vue está funcionando',
+      friends: [
+          {name: 'A', online:true},
+          {name: 'B', online:false},
+          {name: 'C', online:true},
+          {name: 'D', online:false},
+          {name: 'E', online:false},
+      ]
     }
   },
   components:{
     Navbar,
+    AllFriends,
+    OnlineFriends
   },
 }
 </script>
---
+++ vue/src/components/AllFriends.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="allfriends">
+        <h2>Todos Amigos</h2>
+        <div v-for="(friend,index) in friends" :key="index">
+            <h6>{{ friend.name }}</h6>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AllFriends',
+    props: ['friends'],
+    data(){
+        return {
+           
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h6 {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    } 
+</style>
