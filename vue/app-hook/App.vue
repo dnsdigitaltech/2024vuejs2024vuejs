c1f53e6028f5d9e47b2e4dc6e6a91a50e8e423ce
Davi Bernardo <davi.bernardo@sbahq.org>
2024-07-01 16:08:08 -0300

    VueJs - Hooks

---
+++ vue/app-hook/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h2>{{ title }}</h2>
+    <Navbar></Navbar>
+    <Hook></Hook>
+  </div>  
+</template>
+
+<script>
+import Navbar from './components/Navbar';
+import Hook from './components/Hook';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      title: 'Uau, nosso site vue está funcionando',
+    }
+  },
+  components:{
+    Navbar,
+    Hook
+  },
+  methods:{
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/app-hook/components/Hook.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="hook">
+        <h1>{{ title }}</h1>
+        <button @click="changeTitle">Clique aqui</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Hook',
+    data(){
+        return {
+            title: 'Dados principais Hook'
+        }
+    },
+    methods:{
+        changeTitle(){
+            this.title = 'Projeto VueJs'
+        }
+    },
+    beforeCreate(){
+        alert('Before Create Hook')
+    },
+    created() {
+        alert('Created Hook')
+    },
+    beforeMount(){
+        alert('Before Mount Hook')
+    },
+    mounted() {
+        alert('Mounted Hook')
+    },
+    beforeUpdate(){
+        alert('Before update message')
+    }
+}
+</script>
+
+<style>
+ 
+</style>
