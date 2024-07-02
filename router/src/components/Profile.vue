cd99219800ea22f5fd792f2343dee9ed7cc0055d
Davi Bernardo <davi.bernardo@sbahq.org>
2024-07-02 10:14:41 -0300

    VueJs Router Parameter

---
+++ router/src/components/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="profile">
+      <h2>Perfil ({{ UserId }})</h2>
+      
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Profile',
+    data () {
+      return {
+        UserId: this.$route.params.user_id
+      }
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+    h2{
+        color: green;
+    }
+  </style>
+  
