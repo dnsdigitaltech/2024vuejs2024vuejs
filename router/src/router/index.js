8d163a85b04df6a9943fefa19336add74b51f5e9
Davi Bernardo <davi.bernardo@sbahq.org>
2024-07-02 09:29:07 -0300

    VueJs Setting Routes

--- router/src/router/index.js
+++ router/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import About from '@/components/About'
+import Contact from '@/components/Contact'
 
 Vue.use(Router)
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/sobre',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/contato',
+      name: 'Contact',
+      component: Contact
     }
   ]
 })
