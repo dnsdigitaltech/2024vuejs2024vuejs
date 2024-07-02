ece51fd30d899dc57ea90674c61982aa6b1099fc
Davi Bernardo <davi.bernardo@sbahq.org>
2024-07-02 10:25:03 -0300

    VueJs Router Object

--- router/src/components/Profile.vue
+++ router/src/components/Profile.vue
@@ -12,6 +12,14 @@
       return {
         UserId: this.$route.params.user_id
       }
+    },
+    methods:{
+        updateId() {
+            this.UserId = this.$route.params.user_id
+        },
+    },
+    watch:{
+        $route: 'updateId'
     }
   }
   </script>
