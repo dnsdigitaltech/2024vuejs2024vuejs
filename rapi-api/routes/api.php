1cbaefdf8eeb4833e3d51e4877ff5a7a4969cc0e
Davi Bernardo <davi.bernardo@sbahq.org>
2024-07-11 12:31:35 -0300

    RestAPi JWT Token Login Method

--- rapi-api/routes/api.php
+++ rapi-api/routes/api.php
@@ -6,8 +6,22 @@ use App\Http\Controllers\Api\SclassController;
 use App\Http\Controllers\Api\SubjectController;
 use App\Http\Controllers\Api\SectionController;
 use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\AuthController;
 
 Route::apiResource('/class', SclassController::class);
 Route::apiResource('/subject', SubjectController::class);
 Route::apiResource('/section', SectionController::class);
 Route::apiResource('/student', StudentController::class);
+
+Route::group([
+
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
+});
